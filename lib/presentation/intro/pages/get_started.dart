060f8733745875e828e72bcc2baa2a05b4f70c27
NadilGT <rccnadildinsara@gmail.com>
2025-08-17 11:14:36 +0530

    Get started page done

--- lib/presentation/intro/pages/get_started.dart
+++ lib/presentation/intro/pages/get_started.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:spotify/common/widgets/button/basic_app_button.dart';
 import 'package:spotify/core/configs/assets/app_images.dart';
+import 'package:spotify/core/configs/assets/app_vectors.dart';
+import 'package:spotify/core/configs/theme/app_colors.dart';
 
 class GetStartedPage extends StatelessWidget {
   const GetStartedPage({super.key});
@@ -10,18 +14,50 @@ class GetStartedPage extends StatelessWidget {
       body: Stack(
         children: [
           Container(
+            padding: EdgeInsets.symmetric(
+              vertical: 40,
+              horizontal: 40,
+            ),
             decoration: BoxDecoration(
               image: DecorationImage(
                 fit: BoxFit.fill,
-                image: AssetImage(
-                  AppImages.introBG
-                  ) 
-              )
+                image: AssetImage(AppImages.introBG),
+              ),
+            ),
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: SvgPicture.asset(AppVectors.logo),
+                ),
+                Spacer(),
+                Text(
+                  "Enjoy Listening to Music",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(height: 21),
+                Text(
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sagittis enim purus sed phasellus. Cursus ornare id scelerisque aliquam.",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 14,
+                    color: AppColors.grey,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 20,),
+                BasicAppButton(
+                  onPressed: (){}, 
+                  title: 'Get Started'
+                  ),
+              ],
             ),
           ),
-          Container(
-            color: Colors.black.withOpacity(0.15),
-          )
+          Container(color: Colors.black.withOpacity(0.15)),
         ],
       ),
     );
